1b2e5945e58a1be80c5b0a2f72b98370b86e9291
unknown <super.user.hello.world@gmail.com>
2023-08-04 16:05:50 +0530

    upto working

---
+++ app.js
@@ -0,0 +1,7 @@
+function calc(){
+    let weight = document.getElementById('weight')
+    let height = document.getElementById('height')
+    let bmi = (weight.value / height.value**2).toFixed(2)
+    console.log(bmi)
+    let result = document.getElementById('result').innerText=bmi
+}
